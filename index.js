a8be80cbff8d7d4a0416b68fdcff7a60416c772e
Riggs Gorman <riggsgorman@Riggss-MacBook-Pro.local>
2024-06-25 20:39:17 -0500

    For loops corrected.

--- index.js
+++ index.js
@@ -1,39 +1,39 @@
 // Window prompt
-/* let flavor = prompt("Please enter your flavor choices separated by commas.", "vanilla,vanilla,vanilla,strawberry,coffee,coffee");
-if (flavor != null) {
+let flavor = prompt(
+  "Please enter your flavor choices separated by commas.",
+  "vanilla,vanilla,vanilla,strawberry,coffee,coffee"
+);
+/* if (flavor != null) {
   console.table(flavor);
 } */
 
-
 // For loops: vanilla,vanilla,vanilla,strawberry,coffee,coffee
-const flavor = 'vanilla, vanilla, vanilla, strawberry, coffee, coffee'
+// const flavor = "vanilla, vanilla, vanilla, strawberry, coffee, coffee";
 const orders = [
-    { name: 'vanilla', value: 0},
-    { name: 'strawberry', value: 0},
-    { name: 'coffee', value: 0}
-]
+  { name: "vanilla", value: 0 },
+  { name: "strawberry", value: 0 },
+  { name: "coffee", value: 0 },
+];
 
 function makeArray(string) {
-    return string.split(', ')
+  return string.split(", ");
 }
 
 function countFlavors(options, array) {
-    for (key of options) {
-        for (element of array) {
-            if (options.name === element) {
-                options[0].value += 1;
-            }
-        }
-    }
-    return options;
+  // for (key of options) {
+  for (element of array) {
+    orders.map((ele) => {
+      if (ele.name === element) {
+        ele.value++;
+      }
+      return ele;
+    });
+  }
+  // }
+  return options;
 }
 
-for (const flavor in orders) {
-
-}
-
-
-let flavorArray = makeArray(flavor)
-console.log(countFlavors(orders, flavorArray))
-console.log(flavorArray)
+let flavorArray = makeArray(flavor);
+console.table(countFlavors(orders, flavorArray));
+console.log(flavorArray);
 // Console table
